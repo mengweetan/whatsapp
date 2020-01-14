419e86ef705f20529c8a21f4811513696ef24be2
mengweetan <mengwee@dapoltd.com>
2020-01-14 11:27:46 +0800

    0.2

--- wa.py
+++ wa.py
@@ -1,8 +1,11 @@
 import re
 
 #_p = re.compile("\[.+\] (.+:)")
-_p = re.compile("\[\d?\d\/\d?\d\/\d\d, \d?\d:\d\d:\d\d \wM\] (.+:)")
+_p = re.compile("\[(\d?\d\/\d?\d\/\d\d), (\d?\d:\d\d:\d\d \wM)\] (.+:)")
 _array = []
+
+import pandas as pd 
+_data = []
 
 with open('_chat.txt', 'r') as f:
 
@@ -12,15 +15,50 @@ with open('_chat.txt', 'r') as f:
 
 		_r = _p.search(_l)
 		
-		if _r and _r.group(1): 
-			_ll = _r.group(1)
+		if _r and _r.group(3): 
+			_ll = _r.group(3)
 			_ll =_ll.replace('\u202a', '')
 			_ll =_ll.replace('\xa0', ' ')
 			_ll =_ll.replace('\u202c', '')
 
-			if _ll!= 'Class of 1983' not in _ll  : _array.append(_ll[:_ll.find(':')])
-			
+			if _ll!= 'Class of 1983' not in _ll  : 
+				_array.append(_ll[:_ll.find(':')])
+				_data.append([_r.group(1),_r.group(2), _ll[:_ll.find(':')]])
 
+df = pd.DataFrame(_data, columns = ['Date', 'Time', 'Person']) 
+
+def weekday(x):
+	return x.weekday()
+	
+
+
+df['Date'] =  pd.to_datetime(df['Date'], format='%d/%m/%y')
+df['Time'] = pd.to_datetime(df['Time']).dt.strftime('%H:%M:%S')
+df['Weekday'] = df.apply(lambda row: weekday(row['Date']), axis=1)
+df['Hour'] = df['Time'].str[:2] 
+
+print (df.info())
+print (df.head())
+print (df.tail())
+
+
+
+g = df.groupby('Person')['Date'].count().reset_index(name='Msg').sort_values('Msg',ascending=False)
+
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+
+
+fig, ax = plt.subplots()
+ax.set_ylim([0,4000])
+_dt = df.groupby('Weekday').count()['Date'].plot(ax=ax, kind='bar')
+
+fig, ax = plt.subplots()
+_dt = df.groupby('Date').count()['Person'].plot(ax=ax)
+fig, ax = plt.subplots()
+_dt = df.groupby('Hour').count()['Person'].plot.barh(ax=ax)
+#print (_dt)
+#ax = _dt.plot.hist(by="Time",bins=24)
 
 import collections
 
@@ -34,13 +72,13 @@ performs = [ v for k,v in _aa.items()]
 #print (objs)
 #print (performs)
 
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import matplotlib.font_manager as mfm
-import numpy as np
-import matplotlib.pyplot as plt
+
+
 
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 plt.rcParams.update({'font.size': 10})
+
+
 
 fig, ax = plt.subplots()
 y_pos = np.arange(len(objs))
@@ -52,5 +90,9 @@ ax.set_yticklabels(objs)
 ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel('# of Appearance (>'+str(THRESHOLD)+')')
 ax.set_title('Group Chat Activity 10/7/18 - 10/1/20 (noon)')
+
+
+
+
 
 plt.show()
